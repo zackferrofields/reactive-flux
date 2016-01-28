8f53788ae4e185b2667b3c8d2b3fddad02f42719
Zack Ferro Fields <zackferrofields@gmail.com>
2016-01-28 09:30:47 +0000

    RxJS React with remote dev tools

--- examples/alt/src/TodoList.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-require('bootstrap/less/bootstrap.less');
-
-import React    from 'react/addons';
-import TodoList from 'components/TodoList';
-
-React.render(<TodoList />, document.getElementById('todo-list'));
-
